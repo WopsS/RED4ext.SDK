5a731d1c1ade21d786a0d4a838182db6a2ac568b
Octavian Dima <hello@wopss.net>
2020-12-26 22:38:15 +0200

    Add asserts for DynamicBuffer

--- include/RED4ext/DynamicBuffer.hpp
+++ include/RED4ext/DynamicBuffer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <RED4ext/Common.hpp>
 
 namespace RED4ext
 {
@@ -11,4 +12,8 @@ struct DynamicBuffer
     uint32_t capacity;
     uint32_t size;
 };
+
+RED4EXT_ASSERT_SIZE(DynamicBuffer<void*>, 0x10);
+RED4EXT_ASSERT_OFFSET(DynamicBuffer<void*>, capacity, 0x8);
+RED4EXT_ASSERT_OFFSET(DynamicBuffer<void*>, size, 0xC);
 } // namespace RED4ext
