d4382435e33a41fac2b92ba0d117623d1cfb9540
expired6978 <expired6978@gmail.com>
2020-12-29 11:47:20 -0800

    SimpleTypes contained errors but was not actually included anywhere

--- include/RED4ext/Types/SimpleTypes.hpp
+++ include/RED4ext/Types/SimpleTypes.hpp
@@ -35,10 +35,12 @@ RED4EXT_ASSERT_SIZE(CRUIDRef, 0x8);
 
 struct TweakDBID
 {
+#pragma pack(push, 1)
     uint32_t name;  // 00 CRC32
     uint8_t length; // 04
     uint16_t unk05; // 05
     uint8_t unk07;  // 07
+#pragma pack(pop)
 };
 RED4EXT_ASSERT_SIZE(TweakDBID, 0x8);
 
@@ -85,7 +87,7 @@ struct SharedDataBuffer
 };
 RED4EXT_ASSERT_SIZE(SharedDataBuffer, 0x8);
 
-struct serializationDeferredDataBufferType
+struct serializationDeferredDataBuffer
 {
     int64_t unk0;
     int64_t unk8;
@@ -98,7 +100,7 @@ struct serializationDeferredDataBufferType
     int64_t unk48;
     int64_t unk50;
 };
-RED4EXT_ASSERT_SIZE(serializationDeferredDataBufferType, 0x58);
+RED4EXT_ASSERT_SIZE(serializationDeferredDataBuffer, 0x58);
 
 struct gamedataLocKeyWrapper
 {
