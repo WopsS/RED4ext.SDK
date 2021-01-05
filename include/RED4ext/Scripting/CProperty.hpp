8688b27b75d4a6d8bad0e2328a763e05fa577865
Octavian Dima <hello@wopss.net>
2021-01-06 00:27:31 +0200

    Add some flags for CProperty

--- include/RED4ext/Scripting/CProperty.hpp
+++ include/RED4ext/Scripting/CProperty.hpp
@@ -16,9 +16,16 @@ struct CProperty
         uint64_t b0 : 9;
         uint64_t isOut : 1;
         uint64_t isOptional : 1;
-        uint64_t b12 : 10;
+        uint64_t b12 : 4;
+        uint64_t isPrivate : 1;
+        uint64_t isProtected : 1;
+        uint64_t isPublic : 1;
+        uint64_t b19 : 2;
         uint64_t b21 : 1;
-        uint64_t b22 : 42;
+        uint64_t b22 : 5;
+        uint64_t isHandle : 1;
+        uint64_t isPersistent : 1;
+        uint64_t b29 : 36;
     };
     RED4EXT_ASSERT_SIZE(CProperty::Flags, 0x8);
 
