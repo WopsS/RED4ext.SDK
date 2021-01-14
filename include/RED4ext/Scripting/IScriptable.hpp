bce5c4639fae973f6539e28cb26a651798b81851
PixelRick <thispawn@gmail.com>
2021-01-14 22:34:19 +0100

    no exception style return values

--- include/RED4ext/Scripting/IScriptable.hpp
+++ include/RED4ext/Scripting/IScriptable.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <optional>
 
 #include <RED4ext/ISerializable.hpp>
 #include <RED4ext/CName.hpp>
+#include <RED4ext/Types/InstanceType.hpp>
 
 namespace RED4ext
 {
@@ -24,52 +26,48 @@ struct IScriptable : ISerializable
         return classType ? classType : reinterpret_cast<CClass*>(GetNativeType());
     }
 
-    template <typename ReturnT, typename... Args>
-    ReturnT ExecuteFunction(CName aFunc, Args&&... aArgs)
+    template<typename ReturnT = void, typename... Args, typename = std::enable_if_t<!std::is_same_v<ReturnT, void>>>
+    std::optional<ReturnT> ExecuteFunction(CName aFunc, Args&&... aArgs)
     {
-        ReturnT ret {};
-       
-        CClass* cls = GetType();
-        if (!cls)
+        ReturnT ret{};
+
+        if (!ExecuteFunctionImpl<ReturnT>(aFunc, &ret, std::forward<Args>(aArgs)...))
         {
-            return ret;
+            return std::nullopt;
         }
-
-        auto func = cls->GetFunction(aFunc);
-        if (!func)
-        {
-            return ret;
-        }
-
-        StackArgs_t args;
-        ((args.emplace_back(nullptr, &aArgs)), ...);
-        ExecuteFunction(this, func, &ret, args);
-
-        return ret;
+    
+        return std::optional<ReturnT>{ret};
     }
 
-    template<typename... Args>
-    void ExecuteFunction(CName aFunc, Args&&... aArgs)
+    template<typename ReturnT = void, typename... Args, typename = std::enable_if_t<std::is_same_v<ReturnT, void>>>
+    bool ExecuteFunction(CName aFunc, Args&&... aArgs)
     {
-        CClass* cls = GetType();
-        if (!cls)
-        {
-            return;
-        }
-
-        auto func = cls->GetFunction(aFunc);
-        if (!func)
-        {
-            return;
-        }
-
-        StackArgs_t args;
-        ((args.emplace_back(nullptr, &aArgs)), ...);
-        ExecuteFunction(this, func, nullptr, args);
+        return ExecuteFunctionImpl<void>(aFunc, nullptr, std::forward<Args>(aArgs)...);
     }
 
     CClass* classType;   // 30
     void* propertyBlock; // 38
+
+protected:
+    template<typename ReturnType, typename... Args>
+    bool ExecuteFunctionImpl(CName aFunc, ReturnType* ret, Args&&... aArgs)
+    {
+        CClass* cls = GetType();
+        if (!cls)
+        {
+            return false;
+        }
+
+        CBaseFunction* func = cls->GetFunction(aFunc);
+        if (!func)
+        {
+            return false;
+        }
+
+        StackArgs_t args;
+        ((args.emplace_back(nullptr, &aArgs)), ...);
+        return ::RED4ext::ExecuteFunction((void*)this, func, (void*)ret, args);
+    }
 };
 
 RED4EXT_ASSERT_SIZE(IScriptable, 0x40);
