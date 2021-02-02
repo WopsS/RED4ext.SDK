02597bd3837948adc72e970afc76ddaafd04ad47
Octavian Dima <hello@wopss.net>
2021-02-02 23:18:21 +0200

    Include necessary files for including only "RED4ext.hpp"

--- include/RED4ext/RED4ext.hpp
+++ include/RED4ext/RED4ext.hpp
@@ -2,13 +2,25 @@
 
 #include <RED4ext/Common.hpp>
 
-#include <RED4ext/CName.hpp>
+#include <RED4ext/CNamePool.hpp>
+#include <RED4ext/CString.hpp>
+#include <RED4ext/DynArray.hpp>
 #include <RED4ext/GameEngine.hpp>
 #include <RED4ext/Handle.hpp>
+#include <RED4ext/HashMap.hpp>
+#include <RED4ext/ISerializable.hpp>
 #include <RED4ext/MemoryAllocators.hpp>
 #include <RED4ext/RTTISystem.hpp>
+#include <RED4ext/RTTITypes.hpp>
+
+#include <RED4ext/Scripting/CProperty.hpp>
+#include <RED4ext/Scripting/Functions.hpp>
+#include <RED4ext/Scripting/IScriptable.hpp>
+#include <RED4ext/Scripting/Stack.hpp>
 #include <RED4ext/Scripting/Utils.hpp>
 
 #include <RED4ext/Types/GameTime.hpp>
 #include <RED4ext/Types/InstanceType.hpp>
+#include <RED4ext/Types/SharedMutex.hpp>
 #include <RED4ext/Types/SimpleTypes.hpp>
+#include <RED4ext/Types/TweakDB.hpp>
