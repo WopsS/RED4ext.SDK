99c0765488857f894a14efbc6470c77a43690ab8
Octavian Dima <hello@wopss.net>
2021-07-15 16:07:39 +0300

    Add two more virtual functions to "CBaseGameApplication" vtbl and offset comments

--- include/RED4ext/GameApplication.hpp
+++ include/RED4ext/GameApplication.hpp
@@ -19,27 +19,29 @@ enum class EGameStateStatus
 
 struct CBaseGameApplication
 {
-    virtual void sub_0() = 0;
-    virtual void sub_8() = 0;
-    virtual void sub_10() = 0;
-    virtual void sub_18() = 0;
-    virtual void sub_20() = 0;
-    virtual void sub_28() = 0;
-    virtual void sub_30() = 0;
-    virtual void sub_38() = 0;
-    virtual void sub_40() = 0;
-    virtual void sub_48() = 0;
-    virtual void sub_50() = 0;
-    virtual void sub_58() = 0;
-    virtual void sub_60() = 0;
+    virtual void sub_0() = 0;  // 00
+    virtual void sub_8() = 0;  // 08
+    virtual void sub_10() = 0; // 10
+    virtual void sub_18() = 0; // 18
+    virtual void sub_20() = 0; // 20
+    virtual void sub_28() = 0; // 28
+    virtual void sub_30() = 0; // 30
+    virtual void sub_38() = 0; // 38
+    virtual void sub_40() = 0; // 40
+    virtual void sub_48() = 0; // 48
+    virtual void sub_50() = 0; // 50
+    virtual void sub_58() = 0; // 58
+    virtual void sub_60() = 0; // 60
+    virtual void sub_68() = 0; // 68
+    virtual void sub_70() = 0; // 70
 
-    int64_t unk8;
-    int64_t unk10;
-    int64_t unk18;
-    int64_t unk20;
-    CString enginePath;
-    CString r6Path;
-    CString editorPath;
+    int64_t unk8;       // 08
+    int64_t unk10;      // 10
+    int64_t unk18;      // 18
+    int64_t unk20;      // 20
+    CString enginePath; // 28
+    CString r6Path;     // 48
+    CString editorPath; // 68
 };
 RED4EXT_ASSERT_SIZE(CBaseGameApplication, 0x88);
 
@@ -47,36 +49,36 @@ struct CGameApplication : CBaseGameApplication
 {
     struct Unk88
     {
-        int16_t unk0;
-        int8_t unk2;
-        int8_t unk3;
-        int32_t unk4;
-        int64_t unk8;
-        int8_t unk10;
-        int16_t unk12;
-        CString unk18;
-        CString unk38;
-        int64_t unk58;
-        int8_t unk60;
-        int64_t unk68;
-        CString unk70;
-        CString unk90;
-        int32_t unkB0;
-        int32_t unkB4;
-        int8_t unkB8;
-        int32_t unkBC;
-        int8_t unkC0;
+        int16_t unk0;  // 00
+        int8_t unk2;   // 02
+        int8_t unk3;   // 03
+        int32_t unk4;  // 04
+        int64_t unk8;  // 08
+        int8_t unk10;  // 10
+        int16_t unk12; // 12
+        CString unk18; // 18
+        CString unk38; // 38
+        int64_t unk58; // 58
+        int8_t unk60;  // 60
+        int64_t unk68; // 68
+        CString unk70; // 70
+        CString unk90; // 90
+        int32_t unkB0; // B0
+        int32_t unkB4; // B4
+        int8_t unkB8;  // B8
+        int32_t unkBC; // BC
+        int8_t unkC0;  // C0
     };
 
-    Unk88 unk88;
-    DynArray<uint32_t> stateIndexes;
-    DynArray<IGameState*> states;
-    int64_t unk20;
-    IGameState* currState;
-    IGameState* nextState;
-    EGameStateStatus status;
-    char* name;
-    uint32_t result;
+    Unk88 unk88;                     // 88
+    DynArray<uint32_t> stateIndexes; // 150
+    DynArray<IGameState*> states;    // 160
+    int64_t unk170;                  // 170
+    IGameState* currState;           // 178
+    IGameState* nextState;           // 180
+    EGameStateStatus status;         // 188
+    char* name;                      // 190
+    uint32_t result;                 // 198
 };
 RED4EXT_ASSERT_SIZE(CGameApplication, 0x1A0);
 RED4EXT_ASSERT_OFFSET(CGameApplication, currState, 0x178);
