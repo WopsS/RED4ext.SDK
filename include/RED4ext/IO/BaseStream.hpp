5b050f72b4e9f881c00e5490f1399530eb978210
Octavian Dima <hello@wopss.net>
2021-07-20 00:18:12 +0300

    Fix vtable overriding for "MemoryStream"

--- include/RED4ext/IO/BaseStream.hpp
+++ include/RED4ext/IO/BaseStream.hpp
@@ -15,13 +15,13 @@ struct BaseStream
     virtual IMemoryAllocator* GetAllocator(); // 00
     virtual ~BaseStream() = default;          // 08
 
-    virtual bool ReadWrite(void* aBuffer, uint32_t aLength) = 0; // 10
-    virtual LARGE_INTEGER GetPointerPosition() = 0;              // 18
-    virtual LARGE_INTEGER GetLength() = 0;                       // 20
-    virtual bool Seek(LARGE_INTEGER aDistance) = 0;              // 28
-    virtual bool Flush() = 0;                                    // 30
-    virtual void sub_38();                                       // 38
-    virtual const char* GetFileName();                           // 40
+    virtual bool ReadWrite(void* aBuffer, uint32_t aLength); // 10
+    virtual LARGE_INTEGER GetPointerPosition();              // 18
+    virtual LARGE_INTEGER GetLength();                       // 20
+    virtual bool Seek(LARGE_INTEGER aDistance);              // 28
+    virtual bool Flush();                                    // 30
+    virtual void sub_38();                                   // 38
+    virtual const char* GetFileName();                       // 40
 
     template<typename T>
     inline bool ReadWriteEx(T* aBuffer)
