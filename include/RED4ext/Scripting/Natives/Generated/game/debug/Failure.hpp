bd00ad479ddc9abf58dda6fdac86f50bed76ad01
Octavian Dima <hello@wopss.net>
2021-08-16 15:54:24 +0200

    Add missing files because of .gitignore

---
+++ include/RED4ext/Scripting/Natives/Generated/game/debug/Failure.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+// This file is generated from the Game's Reflection data
+
+#include <cstdint>
+#include <RED4ext/Common.hpp>
+#include <RED4ext/Handle.hpp>
+#include <RED4ext/ISerializable.hpp>
+#include <RED4ext/NativeTypes.hpp>
+#include <RED4ext/Scripting/Natives/Generated/game/DebugPath.hpp>
+#include <RED4ext/Scripting/Natives/Generated/game/debug/FailureId.hpp>
+
+namespace RED4ext
+{
+namespace game::debug { struct Failure; }
+
+namespace game::debug { 
+struct Failure : ISerializable
+{
+    static constexpr const char* NAME = "gamedebugFailure";
+    static constexpr const char* ALIAS = NAME;
+
+    game::debug::FailureId id; // 30
+    float time; // 38
+    uint8_t unk3C[0x48 - 0x3C]; // 3C
+    CString message; // 48
+    game::DebugPath path; // 68
+    Handle<game::debug::Failure> previous; // 90
+    Handle<game::debug::Failure> cause; // A0
+};
+RED4EXT_ASSERT_SIZE(Failure, 0xB0);
+} // namespace game::debug
+} // namespace RED4ext
