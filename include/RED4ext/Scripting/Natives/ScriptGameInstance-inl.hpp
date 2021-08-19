42ae241bbb8a4c7c452769afc609dea06ab43b7c
Octavian Dima <hello@wopss.net>
2021-08-19 07:48:07 +0200

    Add a one time check for ScriptGameInstance's size

--- include/RED4ext/Scripting/Natives/ScriptGameInstance-inl.hpp
+++ include/RED4ext/Scripting/Natives/ScriptGameInstance-inl.hpp
@@ -4,13 +4,35 @@
 #include <RED4ext/Scripting/Natives/ScriptGameInstance.hpp>
 #endif
 
+#include <Windows.h>
+#include <mutex>
+
 #include <RED4ext/GameEngine.hpp>
+#include <RED4ext/RTTISystem.hpp>
 
 RED4EXT_INLINE RED4ext::ScriptGameInstance::ScriptGameInstance(GameInstance* aInstance)
     : instance(aInstance)
     , unk8(1)
     , unk10(0)
 {
+    static std::once_flag flag;
+    std::call_once(flag, []() {
+        auto rtti = CRTTISystem::Get();
+        auto gameInstanceType = rtti->GetClass("ScriptGameInstance");
+
+        constexpr auto compiledSize = sizeof(ScriptGameInstance);
+        auto nativeSize = gameInstanceType->GetSize();
+
+        if (compiledSize != nativeSize)
+        {
+            MessageBox(nullptr,
+                       TEXT("The compiled size do not match the native size for ScriptGameInstance. Check the game "
+                            "executable for the native size."),
+                       TEXT("RED4ext.SDK"), MB_ICONWARNING | MB_OK);
+            std::abort();
+        }
+    });
+
     if (!aInstance)
     {
         auto engine = RED4ext::CGameEngine::Get();
