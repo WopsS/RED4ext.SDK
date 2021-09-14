2707ee42a6e7a2c450c31667769a2ff658cebb04
pv <p@siberx.dev>
2021-09-14 15:20:07 +0300

    Update flags for CBaseFunction

--- include/RED4ext/Scripting/Functions.hpp
+++ include/RED4ext/Scripting/Functions.hpp
@@ -44,20 +44,21 @@ struct CBaseFunction : IFunction
     {
         uint32_t isNative : 1;         // 00
         uint32_t isStatic : 1;         // 01
-        uint32_t isEvent : 1;          // 02
-        uint32_t isExec : 1;           // 03
-        uint32_t hasUndefinedBody : 1; // 04
-        uint32_t isTimer : 1;          // 05
-        uint32_t isPrivate : 1;        // 06
-        uint32_t isProtected : 1;      // 07
-        uint32_t isPublic : 1;         // 08
-        uint32_t b9 : 1;               // 09
+        uint32_t isFinal : 1;          // 02
+        uint32_t isEvent : 1;          // 03
+        uint32_t isExec : 1;           // 04
+        uint32_t hasUndefinedBody : 1; // 05 Unused
+        uint32_t isTimer : 1;          // 06 Unused
+        uint32_t isPrivate : 1;        // 07
+        uint32_t isProtected : 1;      // 08
+        uint32_t isPublic : 1;         // 09
         uint32_t b10 : 1;              // 0A
         uint32_t b11 : 1;              // 0B
-        uint32_t isConst : 1;          // 0C
-        uint32_t isQuest : 1;          // 0D
-        uint32_t isThreadsafe : 1;     // 0E
-        uint32_t b15 : 17;             // 0F
+        uint32_t b12 : 1;              // 0C
+        uint32_t isConst : 1;          // 0D
+        uint32_t isQuest : 1;          // 0E
+        uint32_t isThreadsafe : 1;     // 0F
+        uint32_t b16 : 16;             // 10
     };
     RED4EXT_ASSERT_SIZE(CBaseFunction::Flags, 0x4);
 
