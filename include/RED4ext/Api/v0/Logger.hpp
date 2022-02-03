1572ed640076d24c470eec09db57ad83825f23d5
Octavian Dima <hello@wopss.net>
2022-02-03 21:41:21 +0100

    Fix parameter type for some logger functions

--- include/RED4ext/Api/v0/Logger.hpp
+++ include/RED4ext/Api/v0/Logger.hpp
@@ -12,31 +12,31 @@ struct Logger
     void (*TraceW)(PluginHandle aHandle, const wchar_t* aMessage);
     void (*TraceWF)(PluginHandle aHandle, const wchar_t* aFormat, ...);
 
-    void (*Debug)(PluginHandle aHandle, const wchar_t* aMessage);
+    void (*Debug)(PluginHandle aHandle, const char* aMessage);
     void (*DebugF)(PluginHandle aHandle, const char* aFormat, ...);
 
     void (*DebugW)(PluginHandle aHandle, const wchar_t* aMessage);
     void (*DebugWF)(PluginHandle aHandle, const wchar_t* aFormat, ...);
 
-    void (*Info)(PluginHandle aHandle, const wchar_t* aMessage);
+    void (*Info)(PluginHandle aHandle, const char* aMessage);
     void (*InfoF)(PluginHandle aHandle, const char* aFormat, ...);
         
     void (*InfoW)(PluginHandle aHandle, const wchar_t* aMessage);
     void (*InfoWF)(PluginHandle aHandle, const wchar_t* aFormat, ...);
 
-    void (*Warn)(PluginHandle aHandle, const wchar_t* aMessage);
+    void (*Warn)(PluginHandle aHandle, const char* aMessage);
     void (*WarnF)(PluginHandle aHandle, const char* aFormat, ...);
 
     void (*WarnW)(PluginHandle aHandle, const wchar_t* aMessage);
     void (*WarnWF)(PluginHandle aHandle, const wchar_t* aFormat, ...);
 
-    void (*Error)(PluginHandle aHandle, const wchar_t* aMessage);
+    void (*Error)(PluginHandle aHandle, const char* aMessage);
     void (*ErrorF)(PluginHandle aHandle, const char* aFormat, ...);
 
     void (*ErrorW)(PluginHandle aHandle, const wchar_t* aMessage);
     void (*ErrorWF)(PluginHandle aHandle, const wchar_t* aFormat, ...);
 
-    void (*Critical)(PluginHandle aHandle, const wchar_t* aMessage);
+    void (*Critical)(PluginHandle aHandle, const char* aMessage);
     void (*CriticalF)(PluginHandle aHandle, const char* aFormat, ...);
 
     void (*CriticalW)(PluginHandle aHandle, const wchar_t* aMessage);
