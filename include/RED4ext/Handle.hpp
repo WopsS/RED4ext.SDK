e89fbd6bae9a2e5573cc75f42665b36e1539ca7e
pv <p@siberx.dev>
2022-08-02 10:29:45 +0300

    Smart pointers

--- include/RED4ext/Handle.hpp
+++ include/RED4ext/Handle.hpp
@@ -1,115 +1,20 @@
 #pragma once
 
-#include <cstdint>
-#include <memory>
-#include <tuple>
-
-#include <Windows.h>
+#include <type_traits>
 
 #include <RED4ext/Addresses.hpp>
-#include <RED4ext/Common.hpp>
-#include <RED4ext/HashMap.hpp>
 #include <RED4ext/Relocation.hpp>
+#include <RED4ext/SharedPtr.hpp>
 
 namespace RED4ext
 {
+class ISerializable;
+
 template<typename T>
 class WeakHandle;
 
-struct RefCnt
-{
-    volatile uint32_t strongRefs;
-    volatile uint32_t weakRefs;
-
-    void IncRef()
-    {
-        InterlockedIncrement(&strongRefs);
-    }
-
-    // Returns true if the strong refs count has been successfully incremented.
-    bool IncRefIfNotZero()
-    {
-        uint32_t uses = strongRefs;
-        while (uses != 0)
-        {
-            const uint32_t oldUses = InterlockedCompareExchange(&strongRefs, uses + 1, uses);
-            if (oldUses == uses)
-                return true;
-
-            uses = oldUses;
-        }
-
-        return false;
-    }
-
-    // Returns true if the strong refs count reached zero.
-    bool DecRef()
-    {
-        return InterlockedExchangeAdd(&strongRefs, static_cast<uint32_t>(-1)) == 1;
-    }
-
-    void IncWeakRef()
-    {
-        InterlockedIncrement(&weakRefs);
-    }
-};
-
-class HandleBase
-{
-public:
-    HandleBase(const HandleBase&) = delete;
-
-    ~HandleBase() = default;
-
-    HandleBase& operator=(const HandleBase&) = delete;
-
-    [[nodiscard]] uint32_t GetUseCount() const noexcept
-    {
-        return refCount ? refCount->strongRefs : 0;
-    }
-
-    void* instance;
-    RefCnt* refCount;
-
-protected:
-    constexpr HandleBase() noexcept
-        : instance(nullptr)
-        , refCount(nullptr)
-    {
-    }
-
-    void DoSwap(HandleBase& aOther) noexcept
-    {
-        std::swap(instance, aOther.instance);
-        std::swap(refCount, aOther.refCount);
-    }
-
-    void MoveConstructFrom(HandleBase&& aRhs) noexcept
-    {
-        instance = aRhs.instance;
-        refCount = aRhs.refCount;
-        aRhs.instance = nullptr;
-        aRhs.refCount = nullptr;
-    }
-
-    // This is here and not in RefCnt because the function we use expects a pointer to a Handle.
-    void DecWeakRef()
-    {
-        // This function also deallocates RefCnt when weakRefs reaches 0.
-        RelocFunc<void (*)(HandleBase*)> decWeakRefFn(Addresses::Handle_DecWeakRef);
-
-        if (refCount)
-        {
-            decWeakRefFn(this);
-        }
-    }
-};
-RED4EXT_ASSERT_SIZE(HandleBase, 0x10);
-RED4EXT_ASSERT_OFFSET(HandleBase, instance, 0x00);
-RED4EXT_ASSERT_OFFSET(HandleBase, refCount, 0x08);
-
 template<typename T>
-class Handle : public HandleBase
+class Handle : public SharedPtrWithAccess<T>
 {
 public:
     constexpr Handle() noexcept = default;
@@ -117,33 +22,36 @@ public:
     {
     }
 
-    explicit Handle(T* aPtr)
+    explicit Handle(ISerializable* aPtr)
     {
-        RelocFunc<Handle* (*)(Handle*, T*)> ctor(Addresses::Handle_ctor);
+        RelocFunc<Handle* (*)(Handle*, ISerializable*)> ctor(Addresses::Handle_ctor);
         ctor(this, aPtr);
     }
 
     Handle(const Handle& aOther) noexcept
     {
-        aOther.IncRef();
-        instance = aOther.instance;
-        refCount = aOther.refCount;
+        this->CopyConstructFrom(aOther);
     }
 
     Handle(Handle&& aRhs) noexcept
     {
-        MoveConstructFrom(std::move(aRhs));
+        this->MoveConstructFrom(std::move(aRhs));
     }
 
     Handle(const WeakHandle<T>& aOther)
     {
-        if (!TryConstructFromWeak(aOther))
-            throw std::bad_weak_ptr();
+        this->TryCopyConstructFromWeak(aOther);
     }
 
     ~Handle()
     {
-        DecRef();
+        static_assert(std::is_base_of_v<ISerializable, T>,
+                      "Handle only supports types that inherit from ISerializable.");
+
+        if (this->DecRef() && this->instance->CanBeDestructed())
+        {
+            this->Destroy();
+        }
     }
 
     Handle& operator=(const Handle& aRhs) noexcept
@@ -158,35 +66,9 @@ public:
         return *this;
     }
 
-    template<typename U = T, typename = std::enable_if_t<!std::is_void_v<U>>>
-    [[nodiscard]] inline U& operator*() const
-    {
-        return *GetPtr();
-    }
-
-    [[nodiscard]] inline T* operator->() const
-    {
-        return GetPtr();
-    }
-
-    [[nodiscard]] inline operator T*() const
-    {
-        return GetPtr();
-    }
-
-    explicit operator bool() const noexcept
-    {
-        return GetPtr() != nullptr;
-    }
-
-    void Swap(Handle& aOther) noexcept
-    {
-        DoSwap(aOther);
-    }
-
     void Reset() noexcept
     {
-        Handle().swap(*this);
+        Handle().Swap(*this);
     }
 
     void Reset(T* aPtr)
@@ -194,78 +76,40 @@ public:
         Handle(aPtr).Swap(*this);
     }
 
-    [[nodiscard]] T* GetPtr() const noexcept
+    void Swap(Handle& aOther) noexcept
     {
-        return reinterpret_cast<T*>(instance);
-    }
-
-protected:
-    friend class WeakHandle<T>;
-
-    [[nodiscard]] bool TryConstructFromWeak(const WeakHandle<T>& aOther) noexcept
-    {
-        if (aOther.refCount && aOther.refCount->IncRefIfNotZero())
-        {
-            instance = aOther.instance;
-            refCount = aOther.refCount;
-            return true;
-        }
-        return false;
-    }
-
-    void IncRef() const noexcept
-    {
-        if (refCount)
-        {
-            refCount->IncRef();
-        }
-    }
-
-    [[nodiscard]] bool IncRefIfNotZero() noexcept
-    {
-        return refCount ? refCount->IncRefIfNotZero() : false;
-    }
-
-    void DecRef() noexcept
-    {
-        if (refCount && refCount->DecRef())
-        {
-            DecWeakRef();
-            if (IsHandleEmpty(*this))
-            {
-                DeleteHandle(*this);
-                instance = nullptr;
-                refCount = nullptr;
-            }
-        }
+        this->DoSwap(aOther);
     }
 };
-RED4EXT_ASSERT_SIZE(Handle<void*>, 0x10);
+RED4EXT_ASSERT_SIZE(Handle<ISerializable>, 0x10);
 
 template<typename T>
-class WeakHandle : public HandleBase
+class WeakHandle : public WeakPtrWithAccess<T>
 {
 public:
     constexpr WeakHandle() noexcept = default;
 
     WeakHandle(const WeakHandle& aOther) noexcept
     {
-        WeaklyConstructFrom(aOther);
+        this->CopyConstructWeakFrom(aOther);
     }
 
     WeakHandle(const Handle<T>& aOther) noexcept
     {
-        WeaklyConstructFrom(aOther);
+        this->CopyConstructWeakFrom(aOther);
     }
 
     WeakHandle(WeakHandle&& aOther) noexcept
     {
-        MoveConstructFrom(std::move(aOther));
+        this->MoveConstructFrom(std::move(aOther));
     }
 
     ~WeakHandle() noexcept
     {
-        DecWeakRef();
+        static_assert(std::is_base_of_v<ISerializable, T>,
+                      "WeakHandle only supports types that inherit from ISerializable.");
+
+        this->DecWeakRef();
     }
 
     WeakHandle& operator=(const WeakHandle& aRhs) noexcept
@@ -293,51 +137,13 @@ public:
 
     void Swap(WeakHandle& aOther) noexcept
     {
-        DoSwap(aOther);
-    }
-
-    [[nodiscard]] bool Expired() const noexcept
-    {
-        return GetUseCount() == 0;
+        this->DoSwap(aOther);
     }
 
     [[nodiscard]] Handle<T> Lock() const noexcept
     {
-        Handle<T> ret;
-        std::ignore = ret.TryConstructFromWeak(*this);
-        return ret;
-    }
-
-protected:
-    void WeaklyConstructFrom(const HandleBase& aOther) noexcept
-    {
-        instance = aOther.instance;
-        refCount = aOther.refCount;
-        IncWeakRef();
-    }
-
-    void IncWeakRef() const noexcept
-    {
-        if (refCount)
-        {
-            refCount->IncWeakRef();
-        }
+        return *this;
     }
 };
-RED4EXT_ASSERT_SIZE(WeakHandle<void>, 0x10);
-
-template<typename T, typename enabled = void>
-bool IsHandleEmpty(T)
-{
-    static_assert(!std::is_same_v<enabled, void>,
-                  "IsHandleEmpty only works for Handle of types inheriting from ISerializable");
-    return false;
-}
-
-template<typename T, typename enabled = void>
-void DeleteHandle(T)
-{
-    static_assert(!std::is_same_v<enabled, void>,
-                  "DeleteHandle only works for Handle of types inheriting from ISerializable");
-}
+RED4EXT_ASSERT_SIZE(WeakHandle<ISerializable>, 0x10);
 } // namespace RED4ext
---
+++ include/RED4ext/SharedPtr.hpp
@@ -0,0 +1,371 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include <Windows.h>
+
+#include <RED4ext/Addresses.hpp>
+#include <RED4ext/Common.hpp>
+#include <RED4ext/Relocation.hpp>
+#include <RED4ext/Memory/Utils.hpp>
+
+namespace RED4ext
+{
+struct RefCnt
+{
+    using AllocatorType = Memory::RefCountAllocator;
+
+    constexpr RefCnt() noexcept
+        : strongRefs(1)
+        , weakRefs(1)
+    {
+    }
+
+    void IncRef()
+    {
+        InterlockedIncrement(&strongRefs);
+    }
+
+    // Returns true if the strong refs count has been successfully incremented.
+    bool IncRefIfNotZero()
+    {
+        uint32_t uses = strongRefs;
+        while (uses != 0)
+        {
+            const uint32_t oldUses = InterlockedCompareExchange(&strongRefs, uses + 1, uses);
+            if (oldUses == uses)
+                return true;
+
+            uses = oldUses;
+        }
+
+        return false;
+    }
+
+    // Returns true if the strong refs count reached zero.
+    bool DecRef()
+    {
+        return InterlockedExchangeAdd(&strongRefs, -1) == 1;
+    }
+
+    void IncWeakRef()
+    {
+        InterlockedIncrement(&weakRefs);
+    }
+
+    volatile uint32_t strongRefs;
+    volatile uint32_t weakRefs;
+};
+RED4EXT_ASSERT_SIZE(RefCnt, 0x8);
+
+template<typename T>
+class SharedPtrBase
+{
+public:
+    SharedPtrBase(const SharedPtrBase&) = delete;
+
+    ~SharedPtrBase() = default;
+
+    SharedPtrBase& operator=(const SharedPtrBase&) = delete;
+
+    [[nodiscard]] uint32_t GetUseCount() const noexcept
+    {
+        return refCount ? refCount->strongRefs : 0;
+    }
+
+    T* instance;
+    RefCnt* refCount;
+
+protected:
+    constexpr SharedPtrBase() noexcept
+        : instance(nullptr)
+        , refCount(nullptr)
+    {
+    }
+
+    void DoSwap(SharedPtrBase& aOther) noexcept
+    {
+        std::swap(instance, aOther.instance);
+        std::swap(refCount, aOther.refCount);
+    }
+
+    void CopyConstructFrom(const SharedPtrBase& aOther) noexcept
+    {
+        aOther.IncRef();
+        instance = aOther.instance;
+        refCount = aOther.refCount;
+    }
+
+    bool TryCopyConstructFromWeak(const SharedPtrBase& aOther) noexcept
+    {
+        if (aOther.IncRefIfNotZero())
+        {
+            instance = aOther.instance;
+            refCount = aOther.refCount;
+            return true;
+        }
+
+        return false;
+    }
+
+    void MoveConstructFrom(SharedPtrBase&& aOther) noexcept
+    {
+        instance = aOther.instance;
+        refCount = aOther.refCount;
+        aOther.instance = nullptr;
+        aOther.refCount = nullptr;
+    }
+
+    void CopyConstructWeakFrom(const SharedPtrBase& aOther) noexcept
+    {
+        instance = aOther.instance;
+        refCount = aOther.refCount;
+        IncWeakRef();
+    }
+
+    void IncRef() const noexcept
+    {
+        if (refCount)
+        {
+            refCount->IncRef();
+        }
+    }
+
+    [[nodiscard]] bool IncRefIfNotZero() const noexcept
+    {
+        if (refCount)
+        {
+            return refCount->IncRefIfNotZero();
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] bool DecRef() noexcept
+    {
+        if (refCount && refCount->DecRef())
+        {
+            DecWeakRef();
+            return true;
+        }
+
+        return false;
+    }
+
+    void Destroy() noexcept
+    {
+        Memory::Delete(instance);
+
+        instance = nullptr;
+        refCount = nullptr;
+    }
+
+    void IncWeakRef() const noexcept
+    {
+        if (refCount)
+        {
+            refCount->IncWeakRef();
+        }
+    }
+
+    // This is here and not in RefCnt because the function we use expects a pointer to a SharedPtr.
+    void DecWeakRef()
+    {
+        if (refCount)
+        {
+            // This function also deallocates RefCnt when weakRefs reaches 0.
+            RelocFunc<void (*)(SharedPtrBase*)> decWeakRefFn(Addresses::Handle_DecWeakRef);
+            decWeakRefFn(this);
+        }
+    }
+};
+RED4EXT_ASSERT_SIZE(SharedPtrBase<void>, 0x10);
+RED4EXT_ASSERT_OFFSET(SharedPtrBase<void>, instance, 0x00);
+RED4EXT_ASSERT_OFFSET(SharedPtrBase<void>, refCount, 0x08);
+
+template<typename T>
+class SharedPtrWithAccess : public SharedPtrBase<T>
+{
+public:
+    template<typename U = T, typename = std::enable_if_t<!std::is_void_v<U>>>
+    [[nodiscard]] inline U& operator*() const
+    {
+        return *GetPtr();
+    }
+
+    [[nodiscard]] inline T* operator->() const
+    {
+        return GetPtr();
+    }
+
+    [[nodiscard]] inline operator T*() const
+    {
+        return GetPtr();
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return GetPtr() != nullptr;
+    }
+
+    [[nodiscard]] T* GetPtr() const noexcept
+    {
+        return reinterpret_cast<T*>(this->instance);
+    }
+};
+
+template<typename T>
+class WeakPtrWithAccess : public SharedPtrBase<T>
+{
+public:
+    explicit operator bool() const noexcept
+    {
+        return !Expired();
+    }
+
+    [[nodiscard]] bool Expired() const noexcept
+    {
+        return this->GetUseCount() == 0;
+    }
+};
+
+template<typename T>
+class WeakPtr;
+
+template<typename T>
+class SharedPtr : public SharedPtrWithAccess<T>
+{
+public:
+    constexpr SharedPtr() noexcept = default;
+    constexpr SharedPtr(std::nullptr_t) noexcept
+    {
+    }
+
+    explicit SharedPtr(T* aPtr)
+    {
+        this->instance = aPtr;
+        this->refCount = Memory::New<RefCnt>();
+    }
+
+    SharedPtr(const SharedPtr& aOther) noexcept
+    {
+        this->CopyConstructFrom(aOther);
+    }
+
+    SharedPtr(SharedPtr&& aRhs) noexcept
+    {
+        this->MoveConstructFrom(std::move(aRhs));
+    }
+
+    SharedPtr(const WeakPtr<T>& aOther)
+    {
+        this->TryCopyConstructFromWeak(aOther);
+    }
+
+    ~SharedPtr()
+    {
+        static_assert(Memory::detail::HasAnyAllocator<T> && Memory::detail::IsDestructible<T>,
+                      "SharedPtr only supports types that define the allocator type and are destructible "
+                      "(a polymorphic type requires a virtual destructor).");
+
+        if (this->DecRef())
+        {
+            this->Destroy();
+        }
+    }
+
+    SharedPtr& operator=(const SharedPtr& aRhs) noexcept
+    {
+        SharedPtr(aRhs).Swap(*this);
+        return *this;
+    }
+
+    SharedPtr& operator=(SharedPtr&& aRhs) noexcept
+    {
+        SharedPtr(std::move(aRhs)).Swap(*this);
+        return *this;
+    }
+
+    void Reset() noexcept
+    {
+        SharedPtr().Swap(*this);
+    }
+
+    void Reset(T* aPtr)
+    {
+        SharedPtr(aPtr).Swap(*this);
+    }
+
+    void Swap(SharedPtr& aOther) noexcept
+    {
+        this->DoSwap(aOther);
+    }
+};
+RED4EXT_ASSERT_SIZE(SharedPtr<void*>, 0x10);
+
+template<typename T>
+class WeakPtr : public WeakPtrWithAccess<T>
+{
+public:
+    constexpr WeakPtr() noexcept = default;
+
+    WeakPtr(const WeakPtr& aOther) noexcept
+    {
+        this->CopyConstructWeakFrom(aOther);
+    }
+
+    WeakPtr(const SharedPtr<T>& aOther) noexcept
+    {
+        this->CopyConstructWeakFrom(aOther);
+    }
+
+    WeakPtr(WeakPtr&& aOther) noexcept
+    {
+        this->MoveConstructFrom(std::move(aOther));
+    }
+
+    ~WeakPtr() noexcept
+    {
+        static_assert(Memory::detail::HasAnyAllocator<T> && Memory::detail::IsDestructible<T>,
+                      "WeakPtr only supports types that define the allocator type and are destructible "
+                      "(a polymorphic type requires a virtual destructor).");
+
+        this->DecWeakRef();
+    }
+
+    WeakPtr& operator=(const WeakPtr& aRhs) noexcept
+    {
+        WeakPtr(aRhs).swap(*this);
+        return *this;
+    }
+
+    WeakPtr& operator=(WeakPtr&& aRhs) noexcept
+    {
+        WeakPtr(std::move(aRhs)).Swap(*this);
+        return *this;
+    }
+
+    WeakPtr& operator=(const SharedPtr<T>& aRhs) noexcept
+    {
+        WeakPtr(aRhs).Swap(*this);
+        return *this;
+    }
+
+    void Reset() noexcept
+    {
+        WeakPtr().Swap(*this);
+    }
+
+    void Swap(WeakPtr& aOther) noexcept
+    {
+        this->DoSwap(aOther);
+    }
+
+    [[nodiscard]] SharedPtr<T> Lock() const noexcept
+    {
+        return *this;
+    }
+};
+RED4EXT_ASSERT_SIZE(WeakPtr<void>, 0x10);
+} // namespace RED4ext
