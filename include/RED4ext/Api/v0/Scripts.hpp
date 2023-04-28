f34e302a8ec9d80811c168688022a1606d643bcf
Jack Humbert <jack.humb@gmail.com>
2023-04-28 14:09:04 -0400

    adds script system

---
+++ include/RED4ext/Api/v0/Scripts.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <RED4ext/Api/PluginHandle.hpp>
+
+namespace RED4ext::v0
+{
+struct Scripts
+{
+    /**
+     * @brief Attach a hook.
+     *
+     * @param[in]   aHandle     The plugin's handle.
+     * @param[in]   aPath       The path to be added to the redscript compilation - can be a folder or a .reds file
+     *
+     * @return Returns true if the path was added is attached, false otherwise.
+     *
+     * @example
+     *
+     * RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::EMainReason aReason, const
+     * RED4ext::RED4ext* aRED4ext)
+     * {
+     *     switch (aReason)
+     *     {
+     *     case RED4ext::EMainReason::Load:
+     *     {
+     *         aRED4ext->scripts->Add(aHandle, "my_mod.reds");
+     *         break;
+     *     }
+     *     }
+     *
+     *     return true;
+     * }
+     */
+    bool (*Add)(PluginHandle aHandle, std::filesystem::path aPath);
+};
+} // namespace RED4ext::v0
