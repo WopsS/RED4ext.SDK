d017563eb4cb5d875a49e47fe21615bfeee00e1d
pv <p@siberx.dev>
2023-10-31 04:18:13 +0300

    Add convenience overload for Murmur3

--- include/RED4ext/Hashing/Murmur3.hpp
+++ include/RED4ext/Hashing/Murmur3.hpp
@@ -5,6 +5,7 @@
 
 namespace RED4ext
 {
+uint32_t Murmur3_32(const char* aText, const uint32_t aSeed = 0X5EEDBA5E);
 uint32_t Murmur3_32(const uint8_t* aKey, const size_t aLength, const uint32_t aSeed = 0X5EEDBA5E);
 }
 
