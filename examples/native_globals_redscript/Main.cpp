df52d63ccef45deb142c7df9bc1afd58cba5f3c7
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2023-12-05 22:03:43 +0200

    Fix compilation issues

--- examples/native_globals_redscript/Main.cpp
+++ examples/native_globals_redscript/Main.cpp
@@ -92,7 +92,8 @@ RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::
     {
     case RED4ext::EMainReason::Load:
     {
-        RED4ext::RTTIRegistrator::Add(RegisterTypes, PostRegisterTypes);
+        CRTTISystem::Get()->AddRegisterCallback(RegisterTypes);
+        CRTTISystem::Get()->AddPostRegisterCallback(PostRegisterTypes);
         break;
     }
     case RED4ext::EMainReason::Unload:
