1c2c6dd5b1670e63b7fb568c8e062fd479e302cf
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2023-12-05 22:07:57 +0200

    Fix compilation issue due to missing namespace

--- examples/function_registration/Main.cpp
+++ examples/function_registration/Main.cpp
@@ -114,8 +114,10 @@ RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::
     {
     case RED4ext::EMainReason::Load:
     {
-        CRTTISystem::Get()->AddRegisterCallback(RegisterTypes);
-        CRTTISystem::Get()->AddPostRegisterCallback(PostRegisterTypes);
+        auto rtti = RED4ext::CRTTISystem::Get();
+
+        rtti->AddRegisterCallback(RegisterTypes);
+        rtti->AddPostRegisterCallback(PostRegisterTypes);
         break;
     }
     case RED4ext::EMainReason::Unload:
