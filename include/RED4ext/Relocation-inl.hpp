fec5a6821934486fee6d55e2c9739b5e4baa9bf7
psiberx <psiberx@gmail.com>
2024-05-27 18:55:14 +0300

    Fix formatting

--- include/RED4ext/Relocation-inl.hpp
+++ include/RED4ext/Relocation-inl.hpp
@@ -32,39 +32,40 @@ uintptr_t RED4ext::UniversalRelocBase::Resolve(uint32_t aHash)
 
         static std::once_flag flag;
         std::call_once(flag,
-           []()
-           {
-               constexpr auto dllName = "RED4ext.dll";
-               constexpr auto functionName = "RED4ext_ResolveAddress";
+                       []()
+                       {
+                           constexpr auto dllName = "RED4ext.dll";
+                           constexpr auto functionName = "RED4ext_ResolveAddress";
 
-               auto handle = GetModuleHandleA(dllName);
-               if (!handle)
-               {
-                   std::stringstream stream;
-                   stream << "Failed to get '" << dllName
-                          << "' handle.\nProcess will now close.\n\nLast error: " << GetLastError();
+                           auto handle = GetModuleHandleA(dllName);
+                           if (!handle)
+                           {
+                               std::stringstream stream;
+                               stream << "Failed to get '" << dllName
+                                      << "' handle.\nProcess will now close.\n\nLast error: " << GetLastError();
 
-                   MessageBoxA(nullptr, stream.str().c_str(), "RED4ext.SDK", MB_ICONERROR | MB_OK);
-                   TerminateProcess(GetCurrentProcess(), 1);
-               }
+                               MessageBoxA(nullptr, stream.str().c_str(), "RED4ext.SDK", MB_ICONERROR | MB_OK);
+                               TerminateProcess(GetCurrentProcess(), 1);
+                           }
 
-               resolveFunc = reinterpret_cast<functionType>(GetProcAddress(handle, functionName));
-               if (resolveFunc == nullptr)
-               {
-                   std::stringstream stream;
-                   stream << "Failed to get '" << functionName
-                          << "' address.\nProcess will now close.\n\nLast error: " << GetLastError();
+                           resolveFunc = reinterpret_cast<functionType>(GetProcAddress(handle, functionName));
+                           if (resolveFunc == nullptr)
+                           {
+                               std::stringstream stream;
+                               stream << "Failed to get '" << functionName
+                                      << "' address.\nProcess will now close.\n\nLast error: " << GetLastError();
 
-                   MessageBoxA(nullptr, stream.str().c_str(), "RED4ext.SDK", MB_ICONERROR | MB_OK);
-                   TerminateProcess(GetCurrentProcess(), 1);
-               }
-           });
+                               MessageBoxA(nullptr, stream.str().c_str(), "RED4ext.SDK", MB_ICONERROR | MB_OK);
+                               TerminateProcess(GetCurrentProcess(), 1);
+                           }
+                       });
 
         auto address = resolveFunc(aHash);
         if (address == 0)
         {
             std::stringstream stream;
-            stream << "Failed to resolve address for hash " << std::hex << std::showbase << aHash << ".\nProcess will now close.";
+            stream << "Failed to resolve address for hash " << std::hex << std::showbase << aHash
+                   << ".\nProcess will now close.";
 
             MessageBoxA(nullptr, stream.str().c_str(), "RED4ext.SDK", MB_ICONERROR | MB_OK);
             TerminateProcess(GetCurrentProcess(), 1);
