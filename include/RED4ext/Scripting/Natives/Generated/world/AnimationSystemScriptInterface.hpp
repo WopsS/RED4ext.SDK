b57e55a14aceb84ad503521ba1871458d5f1e718
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2024-07-18 14:29:49 -0400

    Create generated AnimationSystemScriptInterface.hpp stub

--- include/RED4ext/Scripting/Natives/Generated/world/AnimationSystemScriptInterface.hpp
+++ include/RED4ext/Scripting/Natives/Generated/world/AnimationSystemScriptInterface.hpp
@@ -4,6 +4,16 @@
 
 // This file is generated from the Game's Reflection data
 
+#include <RED4ext/Scripting/Natives/worldAnimationSystemScriptInterface.hpp>
+
+namespace RED4ext
+{
+RED4EXT_ASSERT_SIZE(world::AnimationSystemScriptInterface, 0x48);
+using worldAnimationSystemScriptInterface = world::AnimationSystemScriptInterface;
+using AnimationSystem = world::AnimationSystemScriptInterface;
+} // namespace RED4ext
+
+/*
 #include <cstdint>
 #include <RED4ext/Common.hpp>
 #include <RED4ext/Scripting/IScriptable.hpp>
@@ -24,5 +34,6 @@ RED4EXT_ASSERT_SIZE(AnimationSystemScriptInterface, 0x48);
 using worldAnimationSystemScriptInterface = world::AnimationSystemScriptInterface;
 using AnimationSystem = world::AnimationSystemScriptInterface;
 } // namespace RED4ext
+*/
 
 // clang-format on
