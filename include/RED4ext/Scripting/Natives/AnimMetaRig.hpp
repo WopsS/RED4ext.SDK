4454ff50effeb50c01f5558d5169246bfa78c34e
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2024-07-19 12:57:01 -0400

    Fix semantics in AnimMetaRig.hpp

--- include/RED4ext/Scripting/Natives/AnimMetaRig.hpp
+++ include/RED4ext/Scripting/Natives/AnimMetaRig.hpp
@@ -13,11 +13,11 @@ struct MetaRig
     DynArray<QsTransform> boneTransforms; // 00
     DynArray<int16_t> parentIndeces;      // 10
     DynArray<CName> boneNames;            // 20
-    DynArray<uint16_t> unkArray30;        // 30
+    DynArray<uint16_t> unk30;             // 30
     DynArray<float> referenceTracks;      // 40
     DynArray<CName> trackNames;           // 50
     uint64_t hash;                        // 60
-    DynArray<uint64_t> unkStructArray;    // 68 array of struct
+    DynArray<uint64_t> unk68;             // 68 - array of struct
 };
 RED4EXT_ASSERT_SIZE(MetaRig, 0x78);
 
