3ef1f854813301ee8eda4166fbe410cc38d00d37
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2024-08-01 00:54:20 -0400

    Add includes in Vector4.hpp

--- include/RED4ext/Scripting/Natives/Vector4.hpp
+++ include/RED4ext/Scripting/Natives/Vector4.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <RED4ext/Common.hpp>
+#include <RED4ext/Scripting/Natives/Vector2.hpp>
+#include <RED4ext/Scripting/Natives/Vector3.hpp>
 #include <cstdint>
 
 namespace RED4ext
