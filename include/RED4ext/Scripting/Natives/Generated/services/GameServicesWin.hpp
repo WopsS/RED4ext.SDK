b6dc8b977091425d4d3c7ce1e80f1d1f1c22fc1f
psiberx <psiberx@gmail.com>
2024-09-12 19:15:08 +0300

    Update runtime version and types

--- include/RED4ext/Scripting/Natives/Generated/services/GameServicesWin.hpp
+++ include/RED4ext/Scripting/Natives/Generated/services/GameServicesWin.hpp
@@ -17,9 +17,9 @@ struct GameServicesWin : services::GameServices
     static constexpr const char* NAME = "servicesGameServicesWin";
     static constexpr const char* ALIAS = NAME;
 
-    uint8_t unk640[0x7C8 - 0x640]; // 640
+    uint8_t unk648[0x7D0 - 0x648]; // 648
 };
-RED4EXT_ASSERT_SIZE(GameServicesWin, 0x7C8);
+RED4EXT_ASSERT_SIZE(GameServicesWin, 0x7D0);
 } // namespace services
 using servicesGameServicesWin = services::GameServicesWin;
 } // namespace RED4ext
