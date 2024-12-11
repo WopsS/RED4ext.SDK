0ae15ef22317f872b036b67938b7a99423b7dccf
psiberx <psiberx@gmail.com>
2024-12-11 12:10:42 +0300

    Fix formatting

--- include/RED4ext/Scripting/Natives/questICharacterConditionType.hpp
+++ include/RED4ext/Scripting/Natives/questICharacterConditionType.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <cstdint>
 #include <RED4ext/Common.hpp>
 #include <RED4ext/Scripting/Natives/Generated/game/EntityReference.hpp>
 #include <RED4ext/Scripting/Natives/Generated/quest/IConditionType.hpp>
+#include <cstdint>
 
 namespace RED4ext
 {
@@ -15,8 +15,8 @@ struct ICharacterConditionType : quest::IConditionType
     static constexpr const char* ALIAS = NAME;
 
     game::EntityReference objectRef; // 38
-    bool isPlayer; // 70
-    uint8_t unk71[0x78 - 0x71]; // 71
+    bool isPlayer;                   // 70
+    uint8_t unk71[0x78 - 0x71];      // 71
 };
 RED4EXT_ASSERT_SIZE(ICharacterConditionType, 0x78);
 } // namespace quest
